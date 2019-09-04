6b9fa7b27815bd9b57eca542bc17b39eeea440f4
Kirankumar Sripati <krnsripati@gmail.com>
2019-09-04 21:00:52 +0530

    unicafe step1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,9 +7,20 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const incrementByOne = (setter, variable) => {
+    return () => { setter(variable + 1) };
+  }
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={incrementByOne(setGood, good)}>good</button>
+      <button onClick={incrementByOne(setNeutral, neutral)}>neutral</button>
+      <button onClick={incrementByOne(setBad, bad)}>bad</button>
+      <h1>statistics</h1>
+      good {good}<br />
+      neutral {neutral}<br />
+      bad {bad}
     </div>
   )
 }
