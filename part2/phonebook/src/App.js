e10cc1ab595b3de23c14e5e5813c2bbb1d666f32
Kirankumar Sripati <krnsripati@gmail.com>
2019-09-07 15:20:41 +0530

    phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,8 +2,13 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
+  const [ filter, setFilter ] = useState('')
+  const [ filteredPersons, setFilteredPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
 
@@ -15,11 +20,22 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = event => {
+    const str = event.target.value;
+    const searchTerm = str.toLowerCase()
+    setFilter(str)
+    if (searchTerm === '') {
+      setFilteredPersons([])
+    } else {
+      setFilteredPersons(persons.filter(p => p.name.toLowerCase().includes(searchTerm)))
+    }
+  }
+
   const addPerson = event => {
     event.preventDefault()
     if (persons.find(p => p.name === newName)) {
       alert(`${newName} is already added to phonebook`)
-      return;
+      return
     }
 
     const person = {
@@ -31,12 +47,19 @@ const App = () => {
     setNewNumber('')
   }
 
-  const getPersons = () =>
-    persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)
+  const getPersons = () => {
+    let personsArr = persons
+    if (filter !== '') {
+      personsArr = filteredPersons;
+    }
+    return personsArr.map(person => <div key={person.name}>{person.name} {person.number}</div>)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter show with <input value={filter} onChange={handleFilterChange} /></div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name:
