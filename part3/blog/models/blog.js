95f8c72bb95edd5ff9f9f79609a017bffe087632
Kirankumar Sripati <krnsripati@gmail.com>
2019-09-15 16:46:05 +0530

    move blog model to seperate file

---
+++ part3/blog/models/blog.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    const object = returnedObject
+    object.id = object._id.toString()
+    delete object._id
+    delete object.__v
+  },
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
