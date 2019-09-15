63bfa9dee8bc8ec468f1164d4bfa1c5bf635932f
Kirankumar Sripati <krnsripati@gmail.com>
2019-09-15 20:09:22 +0530

    4.5*: helper functions and unit tests, step3

--- part3/blog/utils/list_helper.js
+++ part3/blog/utils/list_helper.js
@@ -4,7 +4,18 @@ const dummy = (blogs) => 1
 const totalLikes = (blogs) => blogs
   .reduce((count, blog) => count + blog.likes, 0)
 
+const favoriteBlog = (blogs) => {
+  const favorite = blogs
+    .reduce((prev, cur) => (prev.likes > cur.likes ? prev : cur))
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
