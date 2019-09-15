12ed47ca5c0a0bdd4ffad4d78d577b05e35e4aa7
Kirankumar Sripati <krnsripati@gmail.com>
2019-09-15 16:59:22 +0530

    refactor to app.js and  controllers

--- part3/blog/index.js
+++ part3/blog/index.js
@@ -1,35 +1,9 @@
-const express = require('express')
-
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const http = require('http')
+const app = require('./app')
 const config = require('./utils/config')
-const Blog = require('./models/blog')
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then((blogs) => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result)
-    })
-})
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
