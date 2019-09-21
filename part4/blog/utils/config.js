eaad1e5393c663bc2f9a1ce20ac850a50b0ae9cf
Kirankumar Sripati <krnsripati@gmail.com>
2019-09-22 02:09:37 +0530

    define mongo uri for test

--- part4/blog/utils/config.js
+++ part4/blog/utils/config.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 
-const { PORT, MONGODB_URI } = process.env
+const { PORT, TEST_MONGODB_URI } = process.env
+let { MONGODB_URI } = process.env
+
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = TEST_MONGODB_URI
+}
 
 module.exports = {
   MONGODB_URI,
