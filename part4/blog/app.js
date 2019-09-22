7d3b1d7da15cab72645cbd96bbfe06ad9591dd1b
Kirankumar Sripati <krnsripati@gmail.com>
2019-09-22 13:08:18 +0530

    add logger and unkown end point handler

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -5,24 +5,27 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 
 const app = express()
 
-console.log('connecting to', config.MONGODB_URI)
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
-    console.log('connected to MongoDB')
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
+    logger.error('error connection to MongoDB:', error.message)
   })
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 
+app.use(middleware.unknownEndPoint)
 app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -1,4 +1,20 @@
+const logger = require('./logger')
+
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method)
+  logger.info('Path:  ', request.path)
+  logger.info('Body:  ', request.body)
+  logger.info('---')
+  next()
+}
+
+const unknownEndPoint = (request, response) => {
+  response.status(404).send({ error: 'unkown endpoint' })
+}
+
 const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
   if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
   }
@@ -7,9 +23,11 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message })
   }
 
-  return next(error)
+  next(error)
 }
 
 module.exports = {
+  requestLogger,
+  unknownEndPoint,
   errorHandler,
 }
