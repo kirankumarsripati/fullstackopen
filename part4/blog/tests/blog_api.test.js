f8f407688fcfb04283d114934305f296d4438597
Kirankumar Sripati <krnsripati@gmail.com>
2019-09-22 14:14:15 +0530

    move test case into describe block

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -77,70 +77,72 @@ describe('when there is inititially some blogs saved', () => {
         .expect(400)
     })
   })
-})
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Should you buy One Plus?',
-    author: 'Ruth Irlekar',
-    url: 'https://psychx86.com/ruthsworld/',
-    likes: 121,
-  }
+  describe('addition of a new blog', () => {
+    test('succeeds with a valid data', async () => {
+      const newBlog = {
+        title: 'Should you buy One Plus?',
+        author: 'Ruth Irlekar',
+        url: 'https://psychx86.com/ruthsworld/',
+        likes: 121,
+      }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
 
-  const titles = blogsAtEnd.map((blog) => blog.title)
-  expect(titles).toContain('Should you buy One Plus?')
-})
-
-test('likes should be default to zero if not provided', async () => {
-  const newBlog = {
-    title: 'A blog without likes',
-    author: 'Swami Bhairi',
-    url: 'https://sbs.com',
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect((res) => {
-      delete res.body.id
+      const titles = blogsAtEnd.map((blog) => blog.title)
+      expect(titles).toContain('Should you buy One Plus?')
     })
-    .expect(201, {
-      title: 'A blog without likes',
-      author: 'Swami Bhairi',
-      url: 'https://sbs.com',
-      likes: 0,
+
+    test('likes should be default to zero if not provided', async () => {
+      const newBlog = {
+        title: 'A blog without likes',
+        author: 'Swami Bhairi',
+        url: 'https://sbs.com',
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect((res) => {
+          delete res.body.id
+        })
+        .expect(201, {
+          title: 'A blog without likes',
+          author: 'Swami Bhairi',
+          url: 'https://sbs.com',
+          likes: 0,
+        })
     })
-})
 
-test('blog save should give error if title or url missing', async () => {
-  const blogWithoutTitle = {
-    author: 'Swami Bhairi',
-    url: 'https://sbs.com',
-  }
+    test('blog save should give error if title or url missing', async () => {
+      const blogWithoutTitle = {
+        author: 'Swami Bhairi',
+        url: 'https://sbs.com',
+      }
 
-  await api
-    .post('/api/blogs')
-    .send(blogWithoutTitle)
-    .expect(400)
+      await api
+        .post('/api/blogs')
+        .send(blogWithoutTitle)
+        .expect(400)
 
-  const blogWithoutUrl = {
-    title: 'A blog without title',
-    author: 'Swami Bhairi',
-  }
+      const blogWithoutUrl = {
+        title: 'A blog without title',
+        author: 'Swami Bhairi',
+      }
 
-  await api
-    .post('/api/blogs')
-    .send(blogWithoutUrl)
-    .expect(400)
+      await api
+        .post('/api/blogs')
+        .send(blogWithoutUrl)
+        .expect(400)
+    })
+  })
 })
 
 afterAll(() => {
