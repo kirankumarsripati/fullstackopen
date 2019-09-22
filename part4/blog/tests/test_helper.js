5dea196a483c76f2406efbb38a448aafb26b9883
Kirankumar Sripati <krnsripati@gmail.com>
2019-09-22 22:27:56 +0530

    4.15: bloglist expansion, step4

--- part4/blog/tests/test_helper.js
+++ part4/blog/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -50,8 +51,14 @@ const nonExistingId = async () => {
   return blog._id.toString()
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map((user) => user.toJSON())
+}
+
 module.exports = {
   initialBlogs,
   blogsInDb,
   nonExistingId,
+  usersInDb,
 }
