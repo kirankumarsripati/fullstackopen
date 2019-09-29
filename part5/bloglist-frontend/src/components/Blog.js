c673f3ad83989998b3e4cb5d6f28079a744c855e
Kirankumar Sripati <krnsripati@gmail.com>
2019-09-29 22:11:53 +0530

    5.6* Blog list frontend, step6

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleExpand = () => {
+    setExpanded(!expanded)
+  }
+
+  return (
+    <div style={blogStyle}>
+      { expanded
+      ? (
+        <div>
+          <div onClick={toggleExpand}>{blog.title} {blog.author}</div>
+          <a href={blog.url}>{blog.url}</a> <br />
+          {blog.likes} <button>like</button> <br />
+          added by {blog.user.name}
+        </div>
+      )
+      : (
+        <div onClick={toggleExpand}>
+          {blog.title} {blog.author}
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
