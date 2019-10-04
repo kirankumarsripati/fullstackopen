25a140d3c0fef24e42e7c010629743b6bd01fdb9
Kirankumar Sripati <krnsripati@gmail.com>
2019-10-04 21:02:21 +0530

    6.16 Anecdotes and the backend, step1

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Notification from './components/Notification';
 import Filter from './components/Filter';
+import anecdotesService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
-const App = () => {
+const App = ({ initializeAnecdotes }) => {
+  useEffect(() => {
+    anecdotesService
+      .getAll()
+      .then(anecdotes => initializeAnecdotes(anecdotes))
+  })
+
   return (
     <div>
       <Notification />
@@ -16,4 +25,8 @@ const App = () => {
   )
 }
 
-export default App
+const mapDispatchToProps = {
+  initializeAnecdotes,
+}
+
+export default connect(null, mapDispatchToProps)(App)
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -20,8 +11,6 @@ const asObject = (anecdote) => {
 const sortAnecdotes = function (state) {
   return state.sort((a, b) => b.votes - a.votes)
 }
-
-const initialState = anecdotesAtStart.map(asObject)
 
 export const voteAnecdote = (id) => {
   return {
@@ -37,7 +26,14 @@ export const createAnecdote = (content) => {
   }
 }
 
-const reducer = (state = initialState, action) => {
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT',
+    data: anecdotes,
+  }
+}
+
+const reducer = (state = [], action) => {
   switch (action.type) {
     case 'VOTE':
       const id = action.data.id
@@ -48,6 +44,8 @@ const reducer = (state = initialState, action) => {
       }
       return sortAnecdotes(state.map(a => a.id !== id ? a : updatedAnecdote))
     case 'CREATE_NEW':
+      return sortAnecdotes(state.concat(action.data))
+    case 'INIT':
       return sortAnecdotes(state.concat(action.data))
     default:
       return state
