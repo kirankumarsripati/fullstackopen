8aec4345e4c7374e1ae93b102f711c215573ada6
Kirankumar Sripati <krnsripati@gmail.com>
2019-10-05 14:04:57 +0530

    7.1: routed anecdotes, step1

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,4 +1,9 @@
 import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -6,9 +11,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link to='/' style={padding}>anecdotes</Link>
+      <Link to='/create' style={padding}>create new</Link>
+      <Link to='/about' style={padding}>about</Link>
     </div>
   )
 }
@@ -40,7 +45,7 @@ const Footer = () => (
   <div>
     Anecdote app for <a href='https://courses.helsinki.fi/fi/tkt21009'>Full Stack -sovelluskehitys</a>.
 
-    See <a href='https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js</a> for the source code.
+    See <a href='https://github.com/krnsripati/fullstackopen/blob/master/part7/routed-anecdotes/src/App.js'>https://github.com/krnsripati/fullstackopen/blob/master/part7/routed-anecdotes/src/App.js</a> for the source code.
   </div>
 )
 
@@ -125,10 +130,14 @@ const App = () => {
   return (
     <div>
       <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      <Router>
+        <div>
+          <Menu />
+          <Route exact path="/" render={() => <AnecdoteList anecdotes={anecdotes} />} />
+          <Route exact path="/about" render={() => <About />} />
+          <Route exact path="/create" render={() => <CreateNew addNew={addNew} />} />
+        </div>
+      </Router>
       <Footer />
     </div>
   )
