339f562e7ef47148536945b6eceec8f0787199e7
Kirankumar Sripati <krnsripati@gmail.com>
2019-10-06 15:25:53 +0530

    add option to get details by User ID

--- part7/bloglist-backend/controllers/users.js
+++ part7/bloglist-backend/controllers/users.js
@@ -2,11 +2,31 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.get('/', async (request, response) => {
-  const users = await User
-    .find({})
-    .populate('blogs', { url: 1, title: 1, author: 1 })
-  response.json(users)
+usersRouter.get('/', async (request, response, next) => {
+  try {
+    const users = await User
+      .find({})
+      .populate('blogs', { url: 1, title: 1, author: 1 })
+    response.json(users)
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+usersRouter.get('/:id', async (request, response, next) => {
+  try {
+    const user = await User
+      .findById(request.params.id)
+      .populate('blogs', { url: 1, title: 1, author: 1 })
+    if (user) {
+      response.json(user)
+    } else {
+      response.status(404).end()
+    }
+  } catch (exception) {
+    next(exception)
+  }
+
 })
 
 usersRouter.post('/', async (request, response, next) => {
