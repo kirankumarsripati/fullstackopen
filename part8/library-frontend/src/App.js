63a225862c3af06d57fd827ee3c3b2450f8cd9aa
Kirankumar Sripati <krnsripati@gmail.com>
2019-10-28 21:04:58 +0530

    8.8: Authors view

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,21 +1,38 @@
 import React, { useState } from 'react'
+import { gql } from 'apollo-boost'
+import { useQuery } from 'react-apollo'
+import { Button } from '@material-ui/core'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 
+const ALL_AUTHORS = gql`
+{
+  allAuthors {
+    id
+    name
+    born
+    bookCount
+  }
+}
+`
+
 const App = () => {
   const [page, setPage] = useState('authors')
+
+  const authorResult = useQuery(ALL_AUTHORS)
 
   return (
     <div>
       <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        <Button onClick={() => setPage('authors')}>authors</Button>
+        <Button onClick={() => setPage('books')}>books</Button>
+        <Button onClick={() => setPage('add')}>add book</Button>
       </div>
 
       <Authors
         show={page === 'authors'}
+        result={authorResult}
       />
 
       <Books
--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,34 +1,55 @@
-import React, { useState } from 'react'
+import React from 'react'
+import {
+  Typography,
+  Table,
+  TableHead,
+  TableBody,
+  TableCell,
+  TableRow,
+} from '@material-ui/core'
 
-const Authors = (props) => {
-  if (!props.show) {
+const Authors = ({ show, result }) => {
+  if (!show) {
     return null
   }
-  const authors = []
+
+  if (result.loading) {
+    return <div>Loading...</div>
+  }
+
+  let authors = []
+
+  if (result.data.allAuthors) {
+    authors = result.data.allAuthors
+  }
 
   return (
     <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
+      <Typography variant="h5">
+        authors
+      </Typography>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell>
               born
-            </th>
-            <th>
+            </TableCell>
+            <TableCell>
               books
-            </th>
-          </tr>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
           {authors.map(a =>
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
-            </tr>
+            <TableRow key={a.name}>
+              <TableCell>{a.name}</TableCell>
+              <TableCell>{a.born}</TableCell>
+              <TableCell>{a.bookCount}</TableCell>
+            </TableRow>
           )}
-        </tbody>
-      </table>
+        </TableBody>
+      </Table>
 
     </div>
   )
