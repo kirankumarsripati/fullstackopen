ab67efe87238a5e2f1905ea8f01248aa6a9b0639
Kirankumar Sripati <krnsripati@gmail.com>
2019-10-29 14:50:45 +0530

    8.16 & 8.17 user and logging in, Listing books

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,13 +1,19 @@
 require('dotenv').config()
-const { ApolloServer, UserInputError, gql } = require('apollo-server')
-const uuid = require('uuid/v1')
+const {
+  ApolloServer,
+  UserInputError,
+  AuthenticationError,
+  gql
+} = require('apollo-server')
+const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const Book = require('./models/book')
 const Author = require('./models/author')
+const User = require('./models/user')
 
 mongoose.set('useFindAndModify', false)
 
-const { MONGODB_URI } = process.env
+const { MONGODB_URI, SECRET } = process.env
 
 console.log('connecting to', MONGODB_URI)
 
@@ -35,11 +41,22 @@ const typeDefs = gql`
     bookCount: Int!
   }
 
+  type User {
+    id: ID!
+    username: String!
+    favoriteGenre: String!
+  }
+
+  type Token {
+    value: String!
+  }
+
   type Query {
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
     bookCount: Int!
     authorCount: Int!
+    me: User
   }
 
   type Mutation {
@@ -53,6 +70,14 @@ const typeDefs = gql`
       name: String!
       setBornTo: Int!
     ): Author
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
   }
 `
 
@@ -77,6 +102,9 @@ const resolvers = {
     allAuthors: () => Author.find({}),
     bookCount: () => Book.collection.countDocuments(),
     authorCount: () => Author.collection.countDocuments(),
+    me: (root, args, context) => {
+      return context.currentUser
+    },
   },
   Author: {
     bookCount: async (root) => {
@@ -85,13 +113,17 @@ const resolvers = {
     }
   },
   Mutation: {
-    addBook: async (root, args) => {
+    addBook: async (root, args, { currentUser }) => {
       const {
         title,
         author,
         published,
         genres,
       } = args
+
+      if (!currentUser) {
+        throw new AuthenticationError('Please login to add books')
+      }
 
       // check if book author exists,
       // if not add it to the authors
@@ -127,7 +159,11 @@ const resolvers = {
       }
       return Book.findById(newBook._id).populate('author')
     },
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, {currentUser}) => {
+      if (!currentUser) {
+        throw new AuthenticationError('Please login to change author details')
+      }
+
       const authorExists = await Author.findOne({ name: args.name })
       if (authorExists) {
         authorExists.born = args.setBornTo
@@ -141,6 +177,37 @@ const resolvers = {
       } else {
         return null
       }
+    },
+    createUser: async (root, args) => {
+      const user = new User({
+        username: args.username,
+        favoriteGenre: args.favoriteGenre,
+      })
+      try {
+        await user.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args
+        })
+      }
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({
+        username: args.username
+      })
+
+      if (!user || args.password !== 'secret123') {
+        throw new UserInputError('invalid credentials')
+      }
+
+      const userData = {
+        id: user._id,
+        username: user.username,
+      }
+
+      return {
+        value: jwt.sign(userData, SECRET)
+      }
     }
   }
 }
@@ -148,6 +215,16 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), SECRET
+      )
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  }
 })
 
 server.listen().then(({ url }) => {
